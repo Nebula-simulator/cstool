4ee6b8085e7ae001934e82c092db401acc564e81
Luc van Kessel <18052083+lvkessel@users.noreply.github.com>
2020-07-15 13:13:20 +0200

    Remove full traceback from warnings
    This was a beta development leftover

--- apps/cstool.py
+++ apps/cstool.py
@@ -17,22 +17,6 @@ import argparse
 
 
 
-
-#####################################################################
-import traceback
-import warnings
-import sys
-def warn_with_traceback(message, category, filename, lineno, file=None, line=None):
-	log = file if hasattr(file,'write') else sys.stderr
-	traceback.print_stack(file=log)
-	log.write(warnings.formatwarning(message, category, filename, lineno, line))
-warnings.showwarning = warn_with_traceback
-#####################################################################
-
-
-
-
-
 def compile_kieft_elastic(outfile, material_params, K, P):
 	print("# Computing Mott cross-sections using ELSEPA.")
 	mott_fn = mott_dimfp(material_params, K[K>10*units.eV], threads=4)
