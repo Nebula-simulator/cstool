d3ed6a64b9eb19171e915563b18ea2237c21cbfe
Luc van Kessel <18052083+lvkessel@users.noreply.github.com>
2021-02-25 19:04:15 +0100

    Change version to 1.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
 	name = 'cstool',
-	version = '1.0.2',
+	version = '1.0.3',
 	description = 'Computes cross sections for the Nebula simulator',
 	packages = ['cstool',
 		'cstool.common', 'cstool.dielectric_function', 'cstool.endf',
